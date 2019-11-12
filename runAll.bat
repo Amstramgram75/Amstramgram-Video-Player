ff2c4782ebf62fd08b840159848caa1aaeab1f4e
Amstramgram75 <47279599+Amstramgram75@users.noreply.github.com>
2019-11-12 17:23:10 +0100

    Delete runAll.bat

--- runAll.bat
+++
@@ -1,2 +0,0 @@
-start "" "C:\Users\Laurent\AppData\Local\Programs\Microsoft VS Code\Code.exe" D:/Laurent/Documents/Html/amstramgramVideoPlayer/amstramgramVideoPlayer.code-workspace
-start "" "cmd /k npm run dev"
